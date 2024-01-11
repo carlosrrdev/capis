e89e8ab6a9d3d424b540e0fdb7528074919588c9
carlosrrdev <rr.carlos.dev@gmail.com>
2024-01-10 23:03:49 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CAPIs
+
+## Overview
+
+CAPIs (Carlos API Service) is my personal backend API for several
+of my own personal projects.
+
+CAPIs is fully protected and intended to be used privately. However, you are welcome 
+to clone this repo and build on it for your own projects.
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors')
+const app = express();
+const port = 3000;
+
+
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 100
+})
+
+const dummyData = {
+  "name": "Carlos",
+  "type": "admin",
+  "message": "Success"
+}
+
+app.use(limiter)
+app.use(cors({
+  origin: "http://localhost:3000"
+}))
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send(dummyData);
+});
+
+app.listen(port, () => {
+  console.log(`CAPIs is listening at http://localhost:${port}`);
+});
